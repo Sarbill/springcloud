4c50e7cfa5f5de157cf33fd14a3451722faa6256
Sarbill <719943546@qq.com>
2018-09-04 13:54:45 +0800

    commit

---
+++ eureka-customer-feign/src/main/java/com/mj/demo/eurekacustomerfeign/EurekaCustomerFeignApplication.java
@@ -0,0 +1,12 @@
+package com.mj.demo.eurekacustomerfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCustomerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCustomerFeignApplication.class, args);
+    }
+}
